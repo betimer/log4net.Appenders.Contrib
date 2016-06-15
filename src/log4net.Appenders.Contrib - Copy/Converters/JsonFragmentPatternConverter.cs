e84f3cf2ed8871aafdab2d02386dc4353f04fa44
betimer <betimer@gmail.com>
2016-06-15 16:02:31 +1000

    test remote log

---
+++ src/log4net.Appenders.Contrib - Copy/Converters/JsonFragmentPatternConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using log4net.Core;
+using log4net.Layout.Pattern;
+using log4net.Util;
+using Newtonsoft.Json;
+
+namespace log4net.Appenders.Contrib.Converters
+{
+	public class JsonFragmentPatternConverter : PatternLayoutConverter
+	{
+		protected override void Convert(TextWriter writer, LoggingEvent loggingEvent)
+		{
+			var json = string.Empty;
+			if (loggingEvent.ExceptionObject != null)
+			{
+				json = JsonConvert.SerializeObject(
+					new
+					{
+						Message = loggingEvent.RenderedMessage,
+						Exception = loggingEvent.ExceptionObject
+					});
+			}
+			else
+			{
+				var obj = loggingEvent.MessageObject;
+				if (obj == null || obj is string || obj is SystemStringFormat)
+				{
+					json = JsonConvert.SerializeObject(
+						new
+						{
+							Message = loggingEvent.RenderedMessage
+						},
+						Formatting.None);
+				}
+				else
+				{
+					json = JsonConvert.SerializeObject(obj, Formatting.None);
+				}
+			}
+
+			if (json.StartsWith("{") && json.EndsWith("}"))
+				json = json.Substring(1, json.Length - 2);
+			writer.Write(json);
+		}
+	}
+}
---
+++ src/log4net.Appenders.Contrib - Copy/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("log4net.Appenders.Contrib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("log4net.Appenders.Contrib")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("3.0.1.*")]
